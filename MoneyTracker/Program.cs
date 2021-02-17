edc1dd81cfaf851edf31b71de8cea8c81fcaeb1e
busra-akteke <akteke.b03@htlwienwest.at>
2021-02-17 13:25:05 +0100

    Projektdateien hinzufügen.

---
+++ MoneyTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace MoneyTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
