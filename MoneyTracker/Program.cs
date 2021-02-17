3ea21c1ae0ae757d97724d2baa0bbbfbc1d979b4
busra-akteke <akteke.b03@htlwienwest.at>
2021-02-17 14:08:58 +0100

    sa - as

--- MoneyTracker/Program.cs
+++ MoneyTracker/Program.cs
@@ -7,7 +7,7 @@ namespace MoneyTracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("sa - as");
         }
     }
 }
